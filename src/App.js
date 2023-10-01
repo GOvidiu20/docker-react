f54febb60ae26df45ca136877ffa279a0b18e671
GOvidiu20 <ovidiu.gabor200@gmail.com>
2023-10-01 12:36:49 +0300

    build

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import CustomWebcam from './components/webcam.js'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="App">
+        <div className="App"> 
           {/* <CustomWebcam /> */}
         </div>
         salut
