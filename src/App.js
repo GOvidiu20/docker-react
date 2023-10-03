7b3c381a98285b063fcf81878b90d309639c61f9
GOvidiu20 <ovidiu.gabor200@gmail.com>
2023-10-03 16:56:02 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import './App.css';
 import CustomWebcam from './components/webcam.js'
+import Carousel from './components/carousel';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="App"> 
-          {/* <CustomWebcam /> */}
-        </div>
-        salut
-      </header>
-    </div>
+    <Carousel />
   );
 }
 
---
+++ src/components/carousel.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
+const Carousel = () => {
+  const settings = {
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  return (
+    <div>
+      <h2> Basic Carousel Component</h2>
+      <Slider {...settings}>
+        <div>
+          <h3>SLIDE 1</h3>
+        </div>
+        <div>
+          <h3>SLIDE 2</h3>
+        </div>
+        <div>
+          <h3>SLIDE 3</h3>
+        </div>
+        <div>
+          <h3>SLIDE 4</h3>
+        </div>
+        <div>
+          <h3>SLIDE 5</h3>
+        </div>
+        <div>
+          <h3>SLIDE 6</h3>
+        </div>
+      </Slider>
+    </div>
+  );
+};
+
+export default Carousel;
