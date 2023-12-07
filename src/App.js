351147f1f226ed21069de98bd7e1b3f81e0a9803
Gabor Ovidiu <Ovidiu.GABOR@geminicad.com>
2023-12-07 09:13:20 +0200

    dc

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import logo from './logo.svg';
-import CustomWebcam from './components/webcam.js'
-import Carousel from './components/carousel';
+
 function App() {
   return (
       <div className="App">
-        <Carousel/>
+
       </div>
   );
 }
