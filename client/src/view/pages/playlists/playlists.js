1e17449a9399f8108292c6587fc5d812f38c5620
Gabor Ovidiu <Ovidiu.GABOR@geminicad.com>
2023-12-10 23:57:38 +0200

    improves on playlist page

--- client/src/view/pages/playlists/playlists.js
+++ client/src/view/pages/playlists/playlists.js
@@ -1,17 +1,26 @@
-import React, {useMemo, useState} from 'react';
+import React, { useState} from 'react';
 import CustomLayout from "../../components/Layout";
-import { Container, Row, Card, Button} from 'react-bootstrap';
+import { Container, Card} from 'react-bootstrap';
+import Button from '@mui/material/Button';
+import Add from '@mui/icons-material/Add';
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+
 import DataTable, { createTheme } from 'react-data-table-component';
 import './Playlists.scss';
 export default function Playlists() {
     const [data, setData] = useState([
         { id: 1, title: 'Song 1', created_by: 'Author1', data_added: '11.08.2023' },
-        { id: 1, title: 'Song 1', created_by: 'Author1', data_added: '11.08.2023' },
-        { id: 1, title: 'Song 1', created_by: 'Author1', data_added: '11.08.2023' },
-        { id: 1, title: 'Song 1', created_by: 'Author1', data_added: '11.08.2023' },
-        { id: 1, title: 'Song 1', created_by: 'Author1', data_added: '11.08.2023' },
-        { id: 1, title: 'Song 1', created_by: 'Author1', data_added: '11.08.2023' },
-        { id: 1, title: 'Song 1', created_by: 'Author1', data_added: '11.08.2023' },
+        { id: 1, title: 'Song 2', created_by: 'Author1', data_added: '11.08.2023' },
+        { id: 1, title: 'Song 3', created_by: 'Author1', data_added: '11.08.2023' },
+        { id: 1, title: 'Song 3', created_by: 'Author1', data_added: '11.08.2023' },
+        { id: 1, title: 'Song 4', created_by: 'Author1', data_added: '11.08.2023' },
+        { id: 1, title: 'Song 5', created_by: 'Author1', data_added: '11.08.2023' },
+        { id: 1, title: 'Song 6', created_by: 'Author1', data_added: '11.08.2023' },
         { id: 1, title: 'Song 1', created_by: 'Author1', data_added: '11.08.2023' },
         { id: 1, title: 'Song 1', created_by: 'Author1', data_added: '11.08.2023' },
         { id: 1, title: 'Song 1', created_by: 'Author1', data_added: '11.08.2023' },
@@ -33,6 +42,21 @@ export default function Playlists() {
         {
             name: 'Added date',
             selector: row => row.data_added,
+            sortable: 'true'
+        },
+        {
+            name: 'Actions',
+            cell: () => (
+                <div>
+                    <IconButton aria-label="delete" color="error">
+                        <DeleteIcon/>
+                    </IconButton>
+                    <IconButton aria-label="delete" color="primary">
+                        <EditIcon/>
+                    </IconButton>
+                </div>
+            ),
+            button: true,
         },
     ];
     createTheme('datatableTheme', {
@@ -41,7 +65,7 @@ export default function Playlists() {
             secondary: '#2aa198',
         },
         background: {
-            default: '#1fc85a',
+            default: '#212121',
         },
         context: {
             background: '#cb4b16',
@@ -50,34 +74,69 @@ export default function Playlists() {
         divider: {
             default: 'rgba(255,255,255,.12)',
         },
-        button: {
-            default: '#2aa198',
-            hover: 'rgba(0,0,0,.08)',
-            focus: 'rgba(255,255,255,.12)',
-            disabled: 'rgba(255, 255, 255, .34)',
-        },
     });
     const conditionalRowStyles = [
         {
             when: row => row.id != null,
             style: {
                 borderBottom: 'none',
-                color: 'black',
+                color: 'white',
+                fontSize: '15px',
+                fontFamily: 'Franie Geometric Sans Family'
+            },
+        },
+        {
+            when: row => row.id == null,
+            style: {
+                color: '#a1a1a1',
+                fontSize: '15px',
+                fontFamily: 'Franie Geometric Sans Family'
             },
         },
     ];
+    const [filterText, setFilterText] = useState('');
+
+    const filteredData = data.filter((row) =>
+        Object.values(row).some(
+            (value) =>
+                value && value.toString().toLowerCase().includes(filterText.toLowerCase())
+        )
+    );
+    const actions = (
+        <div className="d-flex">
+            <Button className="mx-3" variant="contained" size="small" style={{ backgroundColor: 'white', color: 'black', borderRadius: '30px' }} startIcon={<Add />}>
+                Add Playlist
+            </Button>
+            <Paper
+                component="form"
+                sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 250, background: '#242424',borderRadius: '30px','&:focus-within': {border: '2px solid white',},}}
+            >
+                <IconButton sx={{ p: '10px', color: 'white' }}>
+                    <SearchIcon />
+                </IconButton>
+                <InputBase
+                    sx={{ ml: 1, flex: 1, color:'#838383' }}
+                    placeholder="Search playlist..."
+                    inputProps={{ 'aria-label': 'Search playlist...' }}
+                    onChange={(e) => setFilterText(e.target.value)}
+                />
+            </Paper>
+        </div>
+    );
+
     return (
         <CustomLayout>
-            <Container className="mt-5">
-                <Card className="container-scrollable border-0">
-                        <DataTable
-                            title="Movie List"
-                            columns={columns}
-                            data={data}
-                            fixedHeader
-                            theme="datatableTheme"
-                            conditionalRowStyles={conditionalRowStyles}
-                        />
+            <Container>
+                <Card className="container-scrollable-playlists border-0">
+                    <DataTable
+                        title={<span style={{ color: 'white' }}>My Playlists</span>}
+                        columns={columns}
+                        data={filteredData}
+                        fixedHeader
+                        actions={actions}
+                        theme="datatableTheme"
+                        conditionalRowStyles={conditionalRowStyles}
+                    />
                 </Card>
             </Container>
         </CustomLayout>
